c642c5b7b4d3830d4a228503ce24a38ebc79b180
daviddpi <piscopodavid97@gmail.com>
2021-11-03 17:17:57 +0100

    inserito database e stampato a schermo

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -10,6 +10,7 @@ class MovieController extends Controller{
     
     public function index(){
         $movies = Movie::all();
+        // dd($movie);
         return view('home', compact('movies'));
 
     }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,19 @@
     
 @section('content')
     <div class="container">
-        <div class="row">
-            <div class="col-12 text-success">
-                <h1>Contenuto</h1>
-            </div>
+        <div class="row g-3">
+            @forelse ($movies as $movie)
+            
+                <div class="col-4">
+                    <h2>Titolo: {{ $movie->title }}</h2>
+                    <h3>Titolo originale: {{ $movie->original_title }}</h3>
+                    <p>Data uscita: {{ $movie->date . " " . $movie->nationality}}</p>
+                    <h5>Voto: {{ $movie->vote }}</h5>
+                </div>
+
+            @empty
+                
+            @endforelse
         </div>
     </div>
 @endsection
