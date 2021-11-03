3ebc67e234b406c954160ee4e6983e8a02cc1624
daviddpi <piscopodavid97@gmail.com>
2021-11-03 16:46:30 +0100

    struttura laravel completata iniziata connessione col db

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
+class MovieController extends Controller{
 
-class MovieController extends Controller
-{
-    //
+    
+    
+    public function index(){
+        $movies = Movie::all();
+        return view('home', compact('movies'));
+
+    }
+
+    public function show(){
+        return view('home');
+
+    }
 }
---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.main')
+
+@section('title', 'home')
+    
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-success">
+                <h1>Contenuto</h1>
+            </div>
+        </div>
+    </div>
+@endsection
